3bd0ae7a4f3012c82bb41cc3cadafe29544def94
adammck <adam.mckaig@gmail.com>
2012-08-18 15:23:44 -0400

    Add specs for Selector[:field].all_xpath

---
+++ spec/selectors/field_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+
+describe Capybara::Selector do
+  describe '[:field]' do
+
+    let :selector do
+      Capybara::Selector.all[:field]
+    end
+
+    # Apply an xpath +expression+ to an +html+ fragment.
+    # Returns an array of the Nokogiri nodes which matched.
+    def apply expression, html
+      Nokogiri::HTML.parse(html).xpath(expression.to_s)
+    end
+
+    # Filter +nodes+ with +selector+, using the +name+ filter with +args+.
+    def filter nodes, selector, name, *args
+      nodes.select do |node|
+
+        # I'd prefer to keep Capybara out of this spec, since we're only testing
+        # selectors, but custom filters expect a Capybara::Node.
+
+        n = Capybara::Node::Simple.new(node)
+        selector.custom_filters[name].call(n, *args)
+      end
+    end
+
+    # Fetch attribute +key+ for each of +nodes+.
+    def attrs nodes, key
+      nodes.map { |node| node[key] }
+    end
+
+
+    describe 'all_xpath' do
+      it 'finds fields' do
+        html = <<-HTML
+          <div></div>
+          <span></span>
+          <input type="button">
+          <input type="checkbox">
+          <input type="file">
+          <input type="password">
+          <input type="radio">
+          <input type="reset">
+          <input type="text">
+          <textarea></textarea>
+          <select></select>
+        HTML
+
+        # Not supported:
+        # <input type="submit">
+        # <input type="hidden">
+        # <input type="image">
+
+        xpath = selector.all_xpath
+        nodes = apply xpath, html
+
+        nodes.length.should == 9
+      end
+    end
+
+    describe 'find_xpath' do
+      it 'finds fields by id' do
+        html = <<-HTML
+          <input id="a" value="1">
+          <input id="b" value="2">
+        HTML
+
+        xpath = selector.find_xpath "a"
+        nodes = apply xpath, html
+
+        attrs(nodes, "value").should == %w[1]
+      end
+
+      it 'finds fields by name' do
+        html = <<-HTML
+          <input name="a" value="1">
+          <input name="a" value="2">
+          <input name="b" value="3">
+        HTML
+
+        xpath = selector.find_xpath "a"
+        nodes = apply xpath, html
+
+        attrs(nodes, "value").should == %w[1 2]
+      end
+
+      it 'finds fields by label' do
+        html = <<-HTML
+          <label for="a">Alpha</label>
+          <input id="a" value="1">
+
+          <label for="b">Beta</label>
+          <input id="b" value="2">
+        HTML
+
+        xpath = selector.find_xpath "Alpha"
+        nodes = apply xpath, html
+
+        attrs(nodes, "value").should == %w[1]
+      end
+    end
+
+    describe 'filters' do
+      it 'by value (:checked, :unchecked)' do
+        html = <<-HTML
+          <input type="checkbox" id="a" checked="checked">
+          <input type="checkbox" id="b" checked>
+          <input type="whatever" id="c">
+          <input type="checkbox" id="d">
+        HTML
+
+        xpath = selector.all_xpath
+        nodes = apply xpath, html
+
+        checked = filter nodes, selector, :checked, true
+        attrs(checked, "id").should == %w[a b]
+
+        unchecked = filter nodes, selector, :unchecked, true
+        attrs(unchecked, "id").should == %w[c d]
+      end
+
+      it 'by value (:with)' do
+        html = <<-HTML
+          <input id="a" value="1">
+          <input id="b" value="1">
+          <input id="c" value="2">
+        HTML
+
+        xpath = selector.all_xpath
+        nodes = apply xpath, html
+
+        matching = filter nodes, selector, :with, "1"
+        attrs(matching, "id").should include "a", "b"
+      end
+    end
+  end
+end
